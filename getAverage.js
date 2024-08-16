757734ae7181e4590cb520a977e08b7c52adedfb
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-08-16 11:57:43 +0100

    solve getAverage and getGrade coding challenge

---
+++ getAverage.js
@@ -0,0 +1,11 @@
+const getAverage = (scores) => {
+    let sum = 0
+    for (let i = 0; i <= scores.length - 1; i++) {
+        sum += scores[i]
+    }
+    const avgScores = sum / scores.length
+    return avgScores
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]))
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]))
